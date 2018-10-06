7a2bec383b93a644ab3c5190359739fb9cd88b95
John Woods <john.alan.woods@gmail.com>
2018-10-06 09:30:20 +0100

    Initial Commit

---
+++ batt/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  batt
+//
+//  Created by John Woods on 06/10/2018.
+//  Copyright © 2018 John Woods. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
