5dc89d46aeaa3e938af175f5dbea122251ab7d79
John Woods <john.alan.woods@gmail.com>
2019-01-16 17:18:01 +0000

    v1.0

--- batt/main.swift
+++ batt/main.swift
@@ -1,12 +1,30 @@
-//
-//  main.swift
-//  batt
-//
-//  Created by John Woods on 06/10/2018.
-//  Copyright © 2018 John Woods. All rights reserved.
-//
-
 import Foundation
+import IOKit.ps
 
-print("Hello, World!")
+enum BatteryError: Error { case error }
 
+do {
+	// Take a snapshot of all the power source info
+	guard let snapshot = IOPSCopyPowerSourcesInfo()?.takeRetainedValue()
+		else { throw BatteryError.error }
+	
+	// Pull out a list of power sources
+	guard let sources: NSArray = IOPSCopyPowerSourcesList(snapshot)?.takeRetainedValue()
+		else { throw BatteryError.error }
+	
+	// For each power source...
+	for ps in sources {
+		// Fetch the information for a given power source out of our snapshot
+		guard let info: NSDictionary = IOPSGetPowerSourceDescription(snapshot, ps as CFTypeRef)?.takeUnretainedValue()
+			else { throw BatteryError.error }
+		
+		// Pull out the name and current capacity
+		if let _ = info[kIOPSNameKey] as? String,
+			let capacity = info[kIOPSCurrentCapacityKey] as? Int,
+			let _ = info[kIOPSMaxCapacityKey] as? Int {
+			print("\(capacity)%")
+		}
+	}
+} catch {
+	fatalError()
+}
